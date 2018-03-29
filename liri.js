a935df0d768782bfe9ca307f79feca78475309ed
shaunaono <shaunaono@gmail.com>
2018-03-28 17:43:23 -0700

    commiting update

--- liri.js
+++ liri.js
@@ -1,2 +1,28 @@
+//Twitter
+var Twitter = require("twitter");
+
+client.post("statuses/update", { status: "" }, function(
+  error,
+  tweet,
+  response
+) {
+  if (error) throw error;
+  console.log(tweet);
+  console.log(response);
+});
+
+//Spotify
+var spotify = require("spotify");
+
+spotify.search(
+  { type: "artist OR album OR track", query: "My search query" },
+  function(err, data) {
+    if (err) {
+      console.log("Error occured: " + err);
+      return;
+    }
+  }
+);
+
 require("dotenv").config();
 console.log(process.env);
